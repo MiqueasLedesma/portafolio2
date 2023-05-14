c4a20bafb1ea6e7a261f6804e3a2aff603aaa66e
Miqueas Ledesma <104766422+MiqueasLedesma@users.noreply.github.com>
2023-05-14 13:22:25 -0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,8 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="es">
-      <Head>
-        <title>Mi Portafolio</title>
-        <link rel="icon" href="icons/port-icon.svg" />
-      </Head>
       <body>
         <Main />
         <NextScript />
